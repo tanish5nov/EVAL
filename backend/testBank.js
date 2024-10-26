9bf79e48260d9ae63680452d6cda143347f905d2
Tanish Sharma <109678180+tanish5nov@users.noreply.github.com>
2024-10-27 00:17:45 +0530

    Update testBank.js

--- backend/testBank.js
+++ backend/testBank.js
@@ -4,7 +4,16 @@ const { connectionString } = require("./connectionString");
 mongoose.connect(connectionString);
 
 const schemaTest = new mongoose.Schema({
-  questionId: [String],
+  title: String,
+  sections: [
+    title: String,
+    questions: [
+    {
+      type: String,
+      ref: "questions"
+    }
+    ]
+  ]
 });
 
 const modelTest = mongoose.model("testBank", schemaTest);
